6aab6ed2e7d103ae0640efd27604b496bec613cb
Karthik AG <karthikjr2004jr@gmail.com>
2026-05-12 23:50:55 +0530

    first commit

---
+++ hello.js
@@ -0,0 +1,16 @@
+let name = "karthik";
+let age = 22;
+let favoriteColor = "black";
+
+let score = 20;
+score = 50;
+const PI = 3.14;  
+
+let number = promt("enter your number");
+console.log(number)
+
+
+console.log(name);
+console.log(age);
+console.log(favoriteColor);
+
