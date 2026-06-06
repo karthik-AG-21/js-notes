73bd253be3b8cccd2935bd7f83eee82eb7afc7c8
Karthik AG <karthikjr2004jr@gmail.com>
2026-06-06 16:04:25 +0530

    added advanced topics

--- deepTopics/setInterval.js
+++ deepTopics/setInterval.js
@@ -1,8 +1,119 @@
-setInterval(() => {
-  console.log("Hello");
-}, 100);
+// setInterval(() => {
+//   console.log("Hello");
+// }, 100);
 
 
-setTimeout(()=>{
-    console.log("guys")
-},3000)
+// setTimeout(()=>{
+//     console.log("guys")
+// },3000)
+
+
+
+// function add (){
+//     console.log("2")
+// }
+
+// function two (){
+//     add()
+//     console.log("1")
+
+// }
+
+// two()
+
+
+// console.log("A")
+
+// setTimeout(()=> {
+//     console.log("B")
+// },0)
+
+// console.log("C")
+
+
+// promises
+
+
+// const task = new promise((resolve)=>{
+//     resolve("done")
+// });
+
+// task.then(data => console.log(data)) 
+
+ // 
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// const a = 10;
+// let b = 10;
+// var c = 10;
+
+// greet();
+
+// function greet() {
+//   console.log("Hello");
+// }
+                  
+
+// setTimeout(() => {
+//   console.log("First");
+// }, 2000);
+
+// console.log("Second");
+
+
+// console.log(1);
+
+// setTimeout(() => {
+//   console.log(2);
+// }, 1000);
+
+// console.log(3);
+
+// setTimeout(() => {
+//   console.log(4);
+// }, 0);
+
+
+// setTimeout(() => {
+//   console.log("Step 1");
+
+//   setTimeout(() => {
+//     console.log("Step 2");
+
+//     setTimeout(() => {
+//       console.log("Step 3");
+//     }, 1000);
+
+//   }, 1000);
+
+// }, 5000);
+
+
+
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+// .then(res => {
+//   console.log(res);
+// });
+
+
+
+
+// let obj ={
+//     name:"karthik",
+//     age: 22,
+//     place:"kerala"
+// }
+
+// console.log(obj)
+
+// let data  = JSON.stringify(obj)
+
+// console.log(data)
+
+// let newObj = JSON.parse(data)
+
+// console.log(newObj)
